43850018d8fb0722f716077d810988cd339a336a
Salty <michaelandryauskas@gmail.com>
2016-04-07 01:59:38 -0400

    Fully Functioning Back And Forth
    All written up and functioning

--- Assets/Scripts/SectionScripts/ObstacleScripts/ObstacleMovementScript.cs
+++ Assets/Scripts/SectionScripts/ObstacleScripts/ObstacleMovementScript.cs
@@ -13,7 +13,16 @@ public class ObstacleMovementScript : ObstacleScript {
 
     public float directBlockChance;
 
-    public Transform pointer;
+    //public Transform pointer;
+
+    public GameObject blockPrefab;
+    public float blockMovementSpeed;
+    public float transitionWaitTime;
+    public Animator arrowAnim;
+
+    float startingPosition;
+    GameObject enteringBlock;
+    GameObject exitingBlock;
 
     float speed;
     float waitTime;
@@ -90,12 +99,12 @@ public class ObstacleMovementScript : ObstacleScript {
             int prevTarget = Mathf.FloorToInt(randomPos * waypoints.Count);
             currentTarget = (prevTarget + 1) % waypoints.Count;
 
-            float legTime = (Vector3.Distance(waypoints[0], waypoints[1]) / baseSpeed + baseWaitTime);
+            float legTime = (Vector3.Distance(waypoints[0], waypoints[1]) / speed + waitTime);
             float totalTravelTime = legTime * waypoints.Count;
             float finalPos = randomPos * totalTravelTime % legTime;
-            if (finalPos < legTime - baseWaitTime)
+            if (finalPos < legTime - waitTime)
             {
-                obstacle.localPosition = Vector3.Lerp(waypoints[prevTarget], waypoints[currentTarget], finalPos / (legTime - baseWaitTime));
+                obstacle.localPosition = Vector3.Lerp(waypoints[prevTarget], waypoints[currentTarget], finalPos / (legTime - waitTime));
             }
             else
             {
@@ -103,34 +112,87 @@ public class ObstacleMovementScript : ObstacleScript {
                 startedWaitTime = Time.time - legTime + finalPos;
             }
         }
-        pointer.eulerAngles = new Vector3(0f, 0f, Vector3.Angle(Vector3.right, obstacle.transform.localPosition - waypoints[currentTarget]));
+
+        startingPosition = waypoints[0].x - blockMovementSpeed * (waitTime * 2f + Vector3.Distance(waypoints[0], waypoints[1]) / speed - transitionWaitTime);
+        exitingBlock = Instantiate(blockPrefab) as GameObject;
+        exitingBlock.transform.SetParent(transform.parent);
+        exitingBlock.transform.localPosition = Vector3.right * 10f;
+        enteringBlock = Instantiate(blockPrefab) as GameObject;
+        enteringBlock.transform.SetParent(transform.parent);
+        float timeToCheckpoint = 0f;
+        if (currentTarget == 1)
+        {
+            //Waiting at Left
+            if (obstacle.transform.localPosition == waypoints[1])
+            {
+                timeToCheckpoint = 2f * waitTime - Time.time + startedWaitTime + Vector3.Distance(waypoints[0], waypoints[1]) / speed;
+            //Moving to Right
+            }else{
+                enteringBlock.transform.localPosition = obstacle.transform.localPosition;
+                return;
+            }
+        }
+        else if (currentTarget == 0)
+        {
+            //Waiting at Right
+            if (obstacle.transform.localPosition == waypoints[0])
+            {
+                timeToCheckpoint = waitTime - Time.time + startedWaitTime;
+            }
+            //Moving to Left
+            else
+            {
+                timeToCheckpoint = waitTime  + Vector3.Distance(waypoints[0], obstacle.transform.localPosition) / speed;
+            }
+        }
+
+        enteringBlock.transform.localPosition = Vector3.left * (Mathf.Abs(waypoints[0].x) + blockMovementSpeed * timeToCheckpoint);
+
+        //pointer.eulerAngles = new Vector3(0f, 0f, Vector3.Angle(Vector3.right, obstacle.transform.localPosition - waypoints[currentTarget]));
     }
 	
 	// Update is called once per frame
 	void Update () {
+        if(exitingBlock != null) exitingBlock.transform.position += Vector3.right * blockMovementSpeed * Time.deltaTime;
         if (waypoints[currentTarget] == obstacle.localPosition)
         {
             if (Time.time - startedWaitTime > waitTime)
             {
                 if (currentTarget == waypoints.Count - 1) currentTarget = 0;
                 else currentTarget++;
-                pointer.eulerAngles = new Vector3(0f, 0f, Vector3.Angle(Vector3.right, obstacle.transform.localPosition - waypoints[currentTarget]));
+                enteringBlock.transform.position += Vector3.right * blockMovementSpeed * Time.deltaTime;
             }
             else
             {
-                pointer.eulerAngles = new Vector3(0f, 0f, Mathf.Lerp(Vector3.Angle(Vector3.right, waypoints[1 - currentTarget] - waypoints[currentTarget]), Vector3.Angle(Vector3.right, waypoints[currentTarget] - waypoints[1 - currentTarget]), (Time.time - startedWaitTime) / waitTime));
+                if (Vector3.Distance(obstacle.transform.position, enteringBlock.transform.position) < blockMovementSpeed * Time.deltaTime) enteringBlock.transform.localPosition = obstacle.transform.localPosition;
+                else enteringBlock.transform.localPosition += Vector3.right * blockMovementSpeed * Time.deltaTime;
+
+                arrowAnim.SetFloat("TimeLeft", 1f - (Time.time - startedWaitTime) / waitTime);
                 return;
             }
         }
         float currentDistance = Vector3.Distance(obstacle.localPosition, waypoints[currentTarget]);
         if (currentDistance <= baseSpeed * Time.deltaTime)
         {
+            arrowAnim.SetFloat("TimeLeft", 1.0f);
+            arrowAnim.SetBool("OnRight", currentTarget == 1);
             startedWaitTime = Time.time;
             obstacle.localPosition = waypoints[currentTarget];
+            enteringBlock.transform.position += Vector3.right * blockMovementSpeed * Time.deltaTime;
+            if (currentTarget == 1)
+            {
+                Destroy(exitingBlock);
+                exitingBlock = enteringBlock;
+                enteringBlock = Instantiate(blockPrefab) as GameObject;
+                enteringBlock.transform.SetParent(transform);
+                enteringBlock.transform.localPosition = Vector3.right * startingPosition;
+            }
         }
         else
         {
             obstacle.localPosition += Vector3.Normalize(waypoints[currentTarget] - obstacle.localPosition) * baseSpeed * Time.deltaTime;
+            if (currentTarget == 1) enteringBlock.transform.localPosition = obstacle.localPosition;
+            else enteringBlock.transform.position += Vector3.right * blockMovementSpeed * Time.deltaTime;
         }
 	}
 }
